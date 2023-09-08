0bc1b3a05992671347f0792538c05b3ae53689c0
Zaky Putra Kresya <zakyputrakresya@gmail.com>
2023-09-08 20:43:17 +0700

    update

--- components/admin/CardSlideshow.js
+++ components/admin/CardSlideshow.js
@@ -1,48 +1,9 @@
+import moment from "moment/moment";
 import React, { useEffect, useState } from "react";
-import Select from "react-select";
-import ExistingImageCard from "./ExistingImageCard";
 
-const CardSlideshow = ({ imageUrl, author, lastUpdate }) => {
+const CardSlideshow = ({ url, author, data_time }) => {
   const [isHovered, setIsHovered] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [newImageUrl, setNewImageUrl] = useState(imageUrl);
-  const [activeTab, setActiveTab] = useState("library"); // "library" or "existingList"
-  const [activeImageId, setActiveImageId] = useState(null);
-
-  const handleImageClick = (id) => {
-    setActiveImageId(id);
-  };
-
-  const hrefOptions = [
-    { label: "Option 1", value: "https://example.com/option1" },
-    { label: "Option 2", value: "https://example.com/option2" },
-    { label: "Option 3", value: "https://example.com/option3" },
-  ];
-
-  const customStyles = {
-    control: (provided, state) => ({
-      ...provided,
-      backgroundColor: state.isFocused ? "#374151" : "#374151",
-      border: "none",
-      color: "white",
-    }),
-    menu: (provided) => ({
-      ...provided,
-      backgroundColor: "#1f2937",
-    }),
-    option: (provided, state) => ({
-      ...provided,
-      backgroundColor: state.isFocused ? "#1f2937" : "transparent",
-      color: state.isSelected ? "white" : "white",
-      "&:hover": {
-        backgroundColor: "#4B5563", // Warna latar belakang saat dihover
-      },
-    }),
-    singleValue: (provided) => ({
-      ...provided,
-      color: "white", // Warna teks untuk opsi yang dipilih
-    }),
-  };
 
   const handleCardClick = () => {
     setIsModalOpen(true);
@@ -53,21 +14,10 @@ const CardSlideshow = ({ imageUrl, author, lastUpdate }) => {
   };
 
   const handleImageChange = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      setNewImageUrl(URL.createObjectURL(file));
-    }
   };
   const handleSaveChanges = () => {
     setIsModalOpen(false);
   };
-
-  const dummyData = [
-    { id: 1, name: "Image 1", url: "/img/png/logo.png" },
-    { id: 2, name: "Image 2", url: "/img/png/logo.png" },
-    { id: 3, name: "Image 3", url: "/img/png/logo.png" },
-    // Tambahkan data gambar lainnya sesuai kebutuhan
-  ];
 
   return (
     <div>
@@ -78,7 +28,7 @@ const CardSlideshow = ({ imageUrl, author, lastUpdate }) => {
         onClick={handleCardClick}
       >
         <div className="relative">
-          <img src={newImageUrl} className="w-full h-20 object-cover" />
+          <img src={url} className="w-full h-20 object-cover" />
           {isHovered && (
             <div className="absolute inset-0 flex items-center justify-center bg-black opacity-70 transition-opacity">
               <p className="text-white font-semibold text-lg">Click to Edit</p>
@@ -90,7 +40,7 @@ const CardSlideshow = ({ imageUrl, author, lastUpdate }) => {
             Author: <span className="font-semibold">{author}</span>
           </p>
           <p className="text-white text-xs">
-            Last Update: <span className="font-semibold">{lastUpdate}</span>
+            Last Update: <span className="font-semibold">{moment(data_time).format("DD-MM-YYYY")}</span>
           </p>
         </div>
       </div>
@@ -103,30 +53,6 @@ const CardSlideshow = ({ imageUrl, author, lastUpdate }) => {
               style={{ maxHeight: 600, overflowY: "scroll" }}
             >
               <div className="mb-4">
-                <div className="flex justify-center mb-2">
-                  <button
-                    onClick={() => setActiveTab("library")}
-                    className={`px-4 py-2 ${
-                      activeTab === "library"
-                        ? "bg-blue-500 text-white"
-                        : "bg-gray-500 text-gray-200"
-                    } rounded-l`}
-                  >
-                    Library
-                  </button>
-                  <button
-                    onClick={() => setActiveTab("existingList")}
-                    className={`px-4 py-2 ${
-                      activeTab === "existingList"
-                        ? "bg-blue-500 text-white"
-                        : "bg-gray-500 text-gray-200"
-                    } rounded-r`}
-                  >
-                    Existing List
-                  </button>
-                </div>
-                {activeTab === "library" && (
-                  <>
                     <label className="block text-gray-200 text-sm font-bold mb-2">
                       New Image File:
                     </label>
@@ -136,29 +62,6 @@ const CardSlideshow = ({ imageUrl, author, lastUpdate }) => {
                       onChange={handleImageChange}
                       className="appearance-none w-full py-2 px-3 text-gray-200 leading-tight focus:outline-none focus:shadow-outline"
                     />
-                  </>
-                )}
-                {activeTab === "existingList" && (
-                  <>
-                    <label className="block text-gray-200 text-sm font-bold mb-2">
-                      List Image:
-                    </label>
-                    <div className="flex flex-wrap -m-2">
-                      {dummyData.map((image) => (
-                        <div
-                          key={image.id}
-                          className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 p-2"
-                        >
-                          <ExistingImageCard
-                            imageUrl={image.url}
-                            isActive={activeImageId === image.id}
-                            onClick={() => handleImageClick(image.id)}
-                          />
-                        </div>
-                      ))}
-                    </div>
-                  </>
-                )}
               </div>
               <div className="flex justify-end">
                 <button
--- pages/admin/index.js
+++ pages/admin/index.js
@@ -4,11 +4,14 @@ import CardSlideshow from "@/components/admin/CardSlideshow";
 import Navbar from "@/components/admin/Navbar";
 import Sidebar from "@/components/admin/Sidebar";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import withAuth from "@/components/Auth/withAuth";
+import { RingLoader } from "react-spinners"
+import { getSlideshowData } from "@/components/CarouselBig";
 
 const AdminDashboard = () => {
   const router = useRouter();
+  const [imageSlideshow, setImageSlideshow] = useState([]);
   const cardData = [
     {
       title: "Card Title 1",
@@ -47,33 +50,11 @@ const AdminDashboard = () => {
       lastUpdate: "2023-09-06",
     },
   ];
-  const cardData2 = [
-    {
-      imageUrl: "/img/jpg/CardTitle.jpeg",
-      author: "John Doe",
-      lastUpdate: "2023-09-01",
-    },
-    {
-      imageUrl: "/img/jpg/CardTitle.jpeg",
-      author: "Jane Smith",
-      lastUpdate: "2023-09-02",
-    },
-    {
-      imageUrl: "/img/jpg/CardTitle.jpeg",
-      author: "Alice Johnson",
-      lastUpdate: "2023-09-03",
-    },
-    {
-      imageUrl: "/img/jpg/CardTitle.jpeg",
-      author: "Bob Wilson",
-      lastUpdate: "2023-09-04",
-    },
-    {
-      imageUrl: "/img/jpg/CardTitle.jpeg",
-      author: "Eve Brown",
-      lastUpdate: "2023-09-05",
-    },
-  ];
+  useEffect(() => {
+    getSlideshowData().then((result) => {
+      setImageSlideshow(result);
+    });
+  }, []);
 
   //   useEffect(() => {
   //     // Lakukan pengalihan (redirect) jika pengguna membuka "/admin"
@@ -94,17 +75,25 @@ const AdminDashboard = () => {
             </div>
           </div>
           <div className="flex overflow-x-auto">
-            {cardData2.map((card, index) => (
-              <div key={index} className="flex-shrink-0 w-1/6 p-4">
-                <CardSlideshow {...card} />
+            {imageSlideshow.length === 0 ? (
+              <div className="fixed inset-0 flex items-center justify-center z-50 bg-black font-semibold text-xl text-white">
+                <RingLoader color="#0894da" size={80} /> Loading...
               </div>
-            ))}
+            ) : (
+              imageSlideshow.map((card, index) => (
+                <div key={index} className="flex-shrink-0 w-1/6 p-4">
+                  <CardSlideshow {...card} />
+                </div>
+              ))
+            )}
           </div>
         </div>
         <div className="container">
           <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 p-4 gap-4 text-black dark:text-white">
             <div className="md:col-span-2 xl:col-span-3">
-              <h3 className="text-lg font-semibold text-center">What's New List</h3>
+              <h3 className="text-lg font-semibold text-center">
+                What's New List
+              </h3>
             </div>
           </div>
           <div className="flex overflow-x-auto">
